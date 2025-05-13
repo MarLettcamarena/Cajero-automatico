4955d2873399693872e6400632075f2f3aee3a2e
Mar__Lett <marlett.becerra5097@alumnos.udg.mx>
2025-05-13 17:32:40 -0600

    codigo.py
    ya está corregido

--- codigo.py
+++ codigo.py
@@ -1,3 +1,36 @@
+def retiro():
+   print(f"el usuario {usuario} esta retirando dinero")
+   retiro=int(input("cantidad que desee retirar: ")) #ingresa la cantidad del retiro
+   print("Se retiro la cantidad de:",retiro)
+   confirmar=input("Desea confirmar el retiro? (s/n): ")#confirmacion del retiro
+   if confirmar == "n":#en caso de que el usuario no quiera confirmar el retiro
+            print("Modificar la cantidad a retirar:")
+            retiro=int(input("Reingresar cantidad a retirar: "))#se le pide al usuario que ingrese nuevamente la cantidad a retirar
+   else:
+            print("Retiro exitoso.")
+            saldo= saldo - retiro #se resta el saldo inicial con el retiro
+def deposito():
+    while True:#para que se repita aunque venga negativo
+       print("depositar dinero")#mensaje de iniciacion 
+       try:#evalue el error
+          cuenta=int(input("cuenta a depositar: ")) #ingresar valores 
+          deposito=int(input("Cantidad a depositar: "))
+       except ValueError:#captura el error
+           print("Error: Ingrese un valor numerico.")  
+           continue
+       if deposito <= 0:
+             print("el monto invalido")
+             continue#continue el ciclo continue desde el inicio
+       else:
+             print(f"Se deposito la cantidad de: {deposito} en la cuenta {cuenta}.")
+             confirmar=input("Desea confirmar el deposito? (s/n): ")
+       if confirmar == "n":#confirmacion del deposito
+            print("Modificar la cuenta o la cantidad a depositar:")#mensaje de modificacion
+            cuenta=input("Reingresar cuenta: ") #usamos nuevamente el nombre de las variables para modificar el valor
+            deposito=input("Reingresar deposito: ")
+       else:
+            print("Deposito exitoso.")#En caso de que todo saliera bien, este mensaje se imprime
+            break #se termina el ciclo
 print("Bienvenido al cajero automatico")#mensaje de bienvenida
 saldo= 24000 #saldo inicial
 print("Por favor ingrese su numero de usuario y NIP")#mensaje de ingreso de datos
@@ -11,30 +44,9 @@ while True:#usamos True para que se siga usando el ciclo hasta que este use la o
       print("usuario consultor:", usuario)
       print("Su saldo es de: $",saldo)#se imprime el saldo
     elif menu==2: #retirar dinero
-        print(f"el usuario {usuario} esta retirando dinero")
-        retiro=int(input("cantidad que desee retirar: ")) #ingresa la cantidad del retiro
-        print("Se retiro la cantidad de:",retiro)
-        confirmar=input("Desea confirmar el retiro? (s/n): ")#confirmacion del retiro
-        if confirmar == "n":#en caso de que el usuario no quiera confirmar el retiro
-            print("Modificar la cantidad a retirar:")
-            retiro=int(input("Reingresar cantidad a retirar: "))#se le pide al usuario que ingrese nuevamente la cantidad a retirar
-        else:
-            print("Retiro exitoso.")
-            saldo= saldo - retiro #se resta el saldo inicial con el retiro
+      retiro()
     elif menu ==3:#depodito de dinero
-         print("depositar dinero")#mensaje de iniciacion 
-         cuenta=int(input("cuenta a depositar: ")) #ingresar valores 
-         deposito=int(input("Cantidad a depositar: "))
-         print(f"Se deposito la cantidad de: {deposito} en la cuenta {cuenta}.")
-         confirmar=input("Desea confirmar el deposito? (s/n): ")
-         if confirmar == "n":#confirmacion del deposito
-            print("Modificar la cuenta o la cantidad a depositar:")#mensaje de modificacion
-            cuenta=input("Reingresar cuenta: ") #usamos nuevamente el nombre de las variables para modificar el valor
-            deposito=input("Reingresar deposito: ")
-         else:
-           print("Deposito exitoso.")#En caso de que todo saliera bien, este mensaje se imprime
-           saldo= saldo + deposito #se suma el saldo inicial con el deposito
-
+      deposito()
     elif menu ==4:#salir del cajero
        print("Gracias por usar el cajero automatico.")
        break #deja de trabajar el programa y se cierra
